85ee8de32a0786179410ea933561c24233bb1cec
moises4 <35942313+moises4@users.noreply.github.com>
2018-02-24 17:13:18 -0500

    Add files via upload

--- part1.m
+++ part1.m
@@ -1,53 +1,48 @@
+%%
+close all; clear all;
 
-close all;
-clc;
-
-imageA = imread('motorcycle.bmp');
-imageB = imread('fish.bmp');
-
-
-imageA = rgb2gray(imageA);
-imageB = rgb2gray(imageB);
+img   = imread('./data/motorcycle.bmp');
+img= rgb2gray(img);
+img = imresize(img,[360, 410]);
 
 
-%Perform 2D FFTs
-fftA = fft2(imageA);
-fftB = fft2(imageB);
+img   = fftshift(img(:,:,1));
+F     = fft2(double(img));
 
-magA = fftA;
+magA = 100*log(1+abs(fftshift(F)));
+figure; imagesc(magA); colormap(gray); title('magnitude spectrum');
 
-magB = fftB;
+phiA = angle(F);
+figure; imagesc(phiA);  colormap(gray); title('phase spectrum');
 
-%Display magnitude and phase of 2D FFTs
-magA = fftshift(magA);
-figure; imshow(magA); title('3');
-% magA = abs(magA);
-% figure; imshow(magA); title('1');
-% magA = log(magA+1);
-figure; imshow(magA); title('2');
+%%
+img2   = imread('./data/fish.bmp');
+img2   = rgb2gray(img2);
+img2   = imresize(img2,[360, 410]);
 
 
-magB = fftshift(magB);
+img   = fftshift(img2(:,:,1));
+F2     = fft2(double(img2));
 
-% magB = abs(magB);
-magB = log(magB+1);
-% magB = mat2gray(magB);
+magB = 100*log(1+abs(fftshift(F2)));
+figure; imagesc(magB); colormap(gray); title('magnitude spectrum');
 
-phiA = angle(fftshift(fftA));
-phiB = angle(fftshift(fftB));
+phiB = angle(F2);
+figure; imagesc(phiB);  colormap(gray); title('phase spectrum');
 
 
-figure, imshow(magA,[24 100000]), colormap gray
-title('Image A FFT2 Magnitude')
-figure, imshow(phiA,[-pi pi]), colormap gray
-title('Image A FFT2 Phase')
-figure, imshow((magB),[24 100000]), colormap gray
-title('Image B FFT2 Magnitude')
-figure, imshow(phiB,[-pi pi]), colormap gray
-title('Image B FFT2 Phase')
-
+%%
 %Switch magnitude and phase of 2D FFTs
-fftC = magA.*exp(1i*phiB);
+fftC = magA.*exp(1i.*phiB);
 fftD = magB.*exp(1i.*phiA);
+
+imageC = ifft2(fftC);
+imageD = ifftshift(ifft2(fftD));
+
+
+figure, imshow((imageC)), colormap gray
+title('Image C  Magnitude')
+figure, imshow((imageD)), colormap(gray); 
+title('Image D  Magnitude')
 
 

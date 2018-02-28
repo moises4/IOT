b585c40231c51eed4a7bb931c2b008b3ab691667
Kyle Jonas <C16Kyle.Jonas@usafa.edu>
2018-02-28 15:00:00 -0500

    Add files via upload
    Updated in class

--- part_ii.m
+++ part_ii.m
@@ -1,40 +1,50 @@
-close all; clc;
+% Create and show the new image resulting from neutralizing the magnitude
+% components for each the following input images separately: (dog), (Einstein), and
+% fish).
+clear; close all; clc;
 
+%% Images
 im1 = imread('./data/dog.bmp');
+im1   = imresize(im1,[360, 410]);
+im1   = double(im1)/255;
 image1 = rgb2gray(im1);
 
 im2 = imread('./data/einstein.bmp');
+im2   = imresize(im2,[360, 410]);
+im2   = double(im2)/255;
 image2 = rgb2gray(im2); 
 
 im3 = imread('./data/fish.bmp');
+im3   = imresize(im3,[360, 410]);
+im3   = double(im3)/255;
 image3 = rgb2gray(im3); 
 
 %% Fequency Domain
-F1 = fft2(double(image1));
-F_Mag1 = abs(F1);
-F_Phase1 = exp(1i*angle(F1));
+F1 = fftshift(fft2(double(image1)));
+F1_Mag = abs(F1);
+F1_Phase = exp(1i*angle(F1));
 
-F2 = fft2(double(image2));
-F_Mag2 = abs(F2);
-F_Phase2 = exp(1i*angle(F2));
+F2 = fftshift(fft2(double(image2)));
+F2_Mag = abs(F2);
+F2_Phase = exp(1i*angle(F2));
 
-F3 = fft2(double(image3));
-F_Mag3 = abs(F3);
-F_Phase3 = exp(1i*angle(F3));
+F3 = fftshift(fft2(double(image3)));
+F3_Mag = abs(F3);
+F3_Phase = exp(1i*angle(F3));
 
 %% Reconstructin
 
-I_Phase1 = ifft2(F_Phase1);
-I_Phase2 = ifft2(F_Phase2);
-I_Phase3 = ifft2(F_Phase3);
+I1_Phase = ifft2(ifftshift(F1_Phase));
+I2_Phase = ifft2(ifftshift(F2_Phase));
+I3_Phase = ifft2(ifftshift(F3_Phase));
 
 %% Display reconstructed images
 
-figure,imagesc(abs(I_Phase1)), colormap gray 
+figure,imagesc(abs(I1_Phase)), colormap gray 
 title('Dog image only by Phase');
 
-figure,imagesc(abs(I_Phase2)), colormap gray 
+figure,imagesc(abs(I2_Phase)), colormap gray 
 title('Einstein image only by Phase');
 
-figure,imagesc(abs(I_Phase3)), colormap gray 
+figure,imagesc(abs(I3_Phase)), colormap gray 
 title('Fish image only by Phase');

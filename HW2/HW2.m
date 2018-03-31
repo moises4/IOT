8ffd34b61b580ec6dd958306f52438c3bc4d8fa2
Kyle Jonas <C16Kyle.Jonas@usafa.edu>
2018-03-30 21:32:32 -0400

    Fixed wavelets
    Now getting 2 different wavelets

--- HW2/HW2.m
+++ HW2/HW2.m
@@ -10,23 +10,21 @@ close all; % closes all figures
 % I already used Matlab GUI to generatre the function
 % (importfileAsColVectors) and uploaded it to the homework folder as well
 disp('Load CSV');
-[emotion,pixels,Usage] = importfileAsColVectors('fer2013.csv',2, 35888 );
+[emotion,pixels,Usage] = importfileAsColVectors('fer2013.csv',2, 35887 );
 pixelsChars = char(pixels);
-disp('process complete');
 
 %% ToDO by students: repeat partitionin and processing until you extract all data pixels
 disp('str2num');
 totalDataSize = 35888;
 
-% pixelsData_Test = str2num(pixelsChars(1:1000,:));
+% pixelsData_Test = str2num(pixelsChars(1:50,:));
 
 % tic
 pixelsData_chunk1 = str2num(pixelsChars(1:10000,:));
 pixelsData_chunk2 = str2num(pixelsChars(10001:20000,:));
 pixelsData_chunk3 = str2num(pixelsChars(20001:30000,:));
-pixelsData_chunk4 = str2num(pixelsChars(30001:35888,:));
+pixelsData_chunk4 = str2num(pixelsChars(30001:35887,:));
 % toc
-disp('process complete');
 
 %% ToDO by students:use matlab syntax to combine the pixels data
 %% be smart and save the pixels data as well before you actually use it for
@@ -45,63 +43,67 @@ allPixelData = [pixelsData_chunk1; pixelsData_chunk2; pixelsData_chunk3; pixelsD
 
 disp('Preprocessing images. This will take a while...');
 
-haarProcessed = [];
 dbProcessed = [];
+coifProcessed = [];
                                 
 for i = 1:totalDataSize                                                   
     reshapedImage = reshape(allPixelData(i, 1:end), [48,48])'; % Reshape image into 48x48 matrix
 %     downSampledImage = reshapedImage(1:2:end, 1:2:end); % Down sample matrix into 24x24 matrix
-    reshapedImage = imrotate(reshapedImage,90);
-    
-    %%Haar Wavelet
-    wname = 'haar';
-    [a, h, v, d] = dwt2(reshapedImage,wname);
-    len = length(a);    
-    waveletImage = reshape(a,[1,len*len]);
-    haarProcessed = [haarProcessed; waveletImage];
-    
+%     reshapedImage = imrotate(reshapedImage,90);
     
     %%Daubechies Wavelet
     wname = 'db1';
     [a, h, v, d] = dwt2(reshapedImage,wname);
-    len2 = length(a);    
-    waveletImage = reshape(a,[1,len2*len2]);
+    len1 = length(a);    
+    waveletImage = reshape(a,[1,len1*len1]);
     dbProcessed = [dbProcessed; waveletImage];
     
+%     imagesc(a);
+%     title('Daubechies');
     
-    if (mod(i, 1000) == 0)
-        fprintf('%d out of %d images processed\n', i, totalDataSize);
-    end
+    %%Coiflets Wavelet
+    wname = 'coif1';
+    [a, h, v, d] = dwt2(reshapedImage,wname);
+    len2 = length(a);    
+    waveletImage = reshape(a,[1,len2*len2]);
+    coifProcessed = [coifProcessed; waveletImage];
+    
+%     imagesc(a);
+%     title('Coiflets');
+   
 end
-
-disp('process complete');
 
 disp('Display Images');
 figure;                                         % plot images
 colormap(gray)                                  % set to grayscale
 for i = 1:25                                    % preview first 25 samples
     subplot(5,5,i)                              % plot them in 6 x 6 grid
-    digit = reshape(haarProcessed(i, 1:end), [len,len])';    % row = 48 x 48 image
+    digit = reshape(dbProcessed(i, 1:end), [len1,len1])';    % row = 48 x 48 image
+    digit = imrotate(digit,270);
     imagesc(digit)                              % show the image
-    title(num2str(haarProcessed(i, 1)))                % show the label
+    title(num2str(dbProcessed(i, 1)))                % show the label
 end
 
 figure;                                         % plot images
 colormap(gray)                                  % set to grayscale
 for i = 1:25                                    % preview first 25 samples
     subplot(5,5,i)                              % plot them in 6 x 6 grid
-    digit = reshape(dbProcessed(i, 1:end), [len2,len2])';    % row = 48 x 48 image
+    digit = reshape(coifProcessed(i, 1:end), [len2,len2])';    % row = 48 x 48 image
+    digit = imrotate(digit,270);
     imagesc(digit)                              % show the image
-    title(num2str(dbProcessed(i, 1)))                % show the label
+    title(num2str(coifProcessed(i, 1)))                % show the label
 end
-
 
 %% ToDO by students: after loopoing , save the wavelets data structure as a preprocessed 
 % dataaset so that you can also use it in future without going through 
 % all previous steps again
 
-csvwrite('haarData.csv',haarProcessed)
-csvwrite('dbData.csv',dbProcessed)
+disp('Write CSV');
+
+csvwrite('dbData.csv',dbProcessed);
+csvwrite('coifData.csv',coifProcessed);
+
+disp('process complete');
 
 %% ToDO by students: Now continue the homework on your own, you should not 
 % need more guidance

46015111beae5a6b9b631d55ec5835801fcd6d11
rjp5403 <33160639+rjp5403@users.noreply.github.com>
2018-04-04 14:22:31 -0400

    Update Iris_.m

--- Iris_.m
+++ Iris_.m
@@ -1,5 +1,3 @@
-% Moises lol
-
 clear;
 clc;
 close all; % closes all figures
@@ -12,7 +10,7 @@ sepalLen = Irisdata(:,2);
 sepalWid = Irisdata(:,3);
 petalLen = Irisdata(:,4);
 petalWid = Irisdata(:,5);
-Species = Irisdata(:,6);
+Species  = Irisdata(:,6);
 
 
 for i = 1:150
@@ -24,11 +22,35 @@ for i = 1:150
     
 end
 
-data = [Id1 sepalLen1 sepalWid1 petalLen1 petalWid1];
+data = [sepalLen1 sepalWid1 petalLen1 petalWid1];
 
 figure;
-scatter(data(:,5), data(:,2), data(:,3), data(:,4));
-k = 5;
+scatter(data(:,1), data(:,2), data(:,3), data(:,4));
+k = 3;
+
 
 [cidx,C,sumd,D] = kmeans(data, k);
 [s,h] = silhouette(data,cidx);
+Species1 = 0;
+Species2 = 0;
+Species3 = 0;
+for i = 1 : 150
+    if round(cidx(i)) == 1
+        Species1 = Species1 +1;
+    end
+    if round(cidx(i)) == 2
+        Species2 = Species2 +1;
+    end
+    if round(cidx(i)) == 3
+        Species3 = Species3 +1;
+    end
+end
+
+Error1 = abs(Species1-50)/50;
+
+Error2 = abs(Species2-50)/50;
+Error3 = abs(Species3-50)/50;
+
+TotalError = (Error1 + Error2 + Error3)/3;
+
+TotalAccuracy = 1-TotalError;

0e29d020aa2778eae5d3e0316998fb12894a8c83
Kyle Jonas <C16Kyle.Jonas@usafa.edu>
2018-04-11 13:36:59 -0400

    ImageCapture

---
+++ Final Project/pythonTest.py
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf200
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red64\green11\blue217;\red255\green255\blue255;\red0\green0\blue0;
+\red200\green20\blue201;\red193\green101\blue28;\red180\green36\blue25;\red46\green174\blue187;}
+{\*\expandedcolortbl;;\cssrgb\c32308\c18668\c88227;\csgray\c100000;\csgray\c0;
+\cssrgb\c83396\c23075\c82664;\cssrgb\c80553\c47366\c13835;\cssrgb\c76409\c21698\c12524;\cssrgb\c20196\c73240\c78250;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs22 \cf2 \cb3 \CocoaLigature0 # Python\cf4 \
+\cf5 import\cf4  tensorflow \cf6 as\cf4  tf\
+hello = tf.constant(\cf7 'Hello, TensorFlow!'\cf4 )\
+sess = tf.Session()\
+\cf8 print\cf4 (sess.run(hello))\
+}
